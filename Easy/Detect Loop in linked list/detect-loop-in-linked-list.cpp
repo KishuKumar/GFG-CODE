6f63a86174e7d18d4340824c31c622e15492f6ac
Kishu Kumar <125544746+KishuKumar@users.noreply.github.com>
2024-03-16 15:41:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Detect Loop in linked list/detect-loop-in-linked-list.cpp
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+//Initial template code for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        // your code here
+        if(head==NULL || head->next==NULL) return false;
+        Node* slow_p=head;
+        Node *fast_p=head;
+        int flag=0;
+        slow_p=slow_p->next;
+        fast_p=fast_p->next->next;
+        while(fast_p!=NULL &&fast_p->next!=NULL)
+        {
+           
+            if(slow_p==fast_p)
+            {
+                flag++;
+                break;
+                
+            }
+            slow_p=slow_p->next;
+            fast_p=fast_p->next->next;
+        
+        }
+        return (flag);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        if(ob.detectLoop(head) )
+            cout<< "True\n";
+        else
+            cout<< "False\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
