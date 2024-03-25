46398a3fdbb9c1af3e934c6b3b97912f261a03ac
Kishu Kumar <125544746+KishuKumar@users.noreply.github.com>
2024-03-25 17:43:43 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Key Pair/key-pair.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	    
+	    std::unordered_set<int> hashSet;
+
+    for (int i = 0; i < n; ++i) {
+        int complement = x - arr[i];
+
+        // Check if the complement exists in the hash set
+        if (hashSet.find(complement) != hashSet.end()) {
+            return 1; // Pair found
+        }
+
+        // Add the current element to the hash set
+        hashSet.insert(arr[i]);
+    }
+
+    return 0;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
